16e276df58e2897324f6c4de0c10741b6e9437f1
Alvaro <edualvardo@hotmail>
2024-01-20 12:20:40 -0500

    20240120-Subir proyecto

---
+++ lib/widgets/top_container.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_task_planner_app/theme/colors/light_colors.dart';
+
+class TopContainer extends StatelessWidget {
+  final double height;
+  final double width;
+  final Widget child;
+  final EdgeInsets padding;
+  TopContainer(
+      {this.height = 0,
+      required this.width,
+      required this.child,
+      this.padding = const EdgeInsets.all(0.0)}); // Lo Modifique
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding:
+          padding != null ? padding : EdgeInsets.symmetric(horizontal: 20.0),
+      decoration: BoxDecoration(
+          color: LightColors.kDarkYellow,
+          borderRadius: BorderRadius.only(
+            bottomRight: Radius.circular(40.0),
+            bottomLeft: Radius.circular(40.0),
+          )),
+      height: height,
+      width: width,
+      child: child,
+    );
+  }
+}
